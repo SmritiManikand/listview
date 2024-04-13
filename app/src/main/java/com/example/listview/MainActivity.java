04166ad44ce8d32f4be4936723ca54c89cded269
SmritiManikand <113674204+SmritiManikand@users.noreply.github.com>
2024-04-13 14:12:21 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.listview;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView simpleList;
+    String countryList[] = {"India", "China", "Australia", "Portugal", "America", "Russia","Africa","Germany","France","Spain"};
+    int flags[] = {R.drawable.india, R.drawable.china, R.drawable.australia, R.drawable.portugal, R.drawable.america, R.drawable.russia,R.drawable.africa,R.drawable.germany,R.drawable.france,R.drawable.spain};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleList = (ListView) findViewById(R.id.simpleListView);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), countryList, flags);
+        simpleList.setAdapter(customAdapter);
     }
 }
