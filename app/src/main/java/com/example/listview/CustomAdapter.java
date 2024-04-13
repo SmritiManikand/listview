8d203a16cb1dd8d064cf2518dfb4ad184b23cb2b
SmritiManikand <113674204+SmritiManikand@users.noreply.github.com>
2024-04-13 14:26:47 +0530

    Update CustomAdapter.java

--- app/src/main/java/com/example/listview/CustomAdapter.java
+++ app/src/main/java/com/example/listview/CustomAdapter.java
@@ -1,21 +1,49 @@
 package com.example.listview;
 
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import android.os.Bundle;
-import android.widget.ListView;
+public class CustomAdapter extends BaseAdapter {
+    Context context;
+    String countryList[];
+    int flags[];
+    LayoutInflater inflter;
 
-public class MainActivity extends AppCompatActivity {
-    ListView simpleList;
-    String countryList[] = {"India", "China", "Australia", "Portugal", "America", "Russia","Africa","Germany","France","Spain"};
-    int flags[] = {R.drawable.india, R.drawable.china, R.drawable.australia, R.drawable.portugal, R.drawable.america, R.drawable.russia,R.drawable.africa,R.drawable.germany,R.drawable.france,R.drawable.spain};
+    public CustomAdapter(Context applicationContext, String[] countryList, int[] flags) {
+        this.context = context;
+        this.countryList = countryList;
+        this.flags = flags;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        simpleList = (ListView) findViewById(R.id.simpleListView);
-        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), countryList, flags);
-        simpleList.setAdapter(customAdapter);
+    public int getCount() {
+        return countryList.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.activity_listview, null);
+        TextView country = (TextView)           view.findViewById(R.id.textView);
+        ImageView icon = (ImageView) view.findViewById(R.id.icon);
+        country.setText(countryList[i]);
+        icon.setImageResource(flags[i]);
+        return view;
     }
 }
